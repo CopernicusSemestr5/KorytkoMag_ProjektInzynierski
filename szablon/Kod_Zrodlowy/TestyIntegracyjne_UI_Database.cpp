3e32b616d0bd9b879cdd6813a20790225071bf64
Korycinski Pawel <pawel.korycinski@nokia.com>
2020-02-10 22:17:19 +0100

    Add listing for integ ration tests

---
+++ szablon/Kod_Zrodlowy/TestyIntegracyjne_UI_Database.cpp
@@ -0,0 +1,15 @@
+TEST_F(UIAndDatabaseIntegrationShould, Put_IOPaths_ByOT_BufferToSend_Once)
+{
+   const auto& returnCode = ui_->runPredefinedCommands({
+      {PUT, IOPATHS_OT, "bufferToSend", BUFFER_TO_SEND_VAL_1}
+   });
+
+    auto& received = db_.getObj<IOPaths>(IOPATHS_1_UK);
+
+   ASSERT_TRUE(returnCode);
+   ASSERT_TRUE(received);
+   ASSERT_TRUE(received->bufferToSend);
+   ASSERT_EQ(
+      received->bufferToSend.value(),
+      BUFFER_TO_SEND_VAL_1);
+}
